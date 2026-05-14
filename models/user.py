b5b5586bed848a57b9b8be0d3a60be41bb23d607
malakelbasha78 <malakelbasha789@gmail.com>
2026-05-14 10:25:53 +0300

    first commit

---
+++ models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Enum, DateTime
+from sqlalchemy.sql import func
+from database.db import Base
+import enum
+
+class UserRole(str, enum.Enum):
+    admin = "admin"
+    author = "author"
+    reader = "reader"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(UserRole), default=UserRole.reader)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
